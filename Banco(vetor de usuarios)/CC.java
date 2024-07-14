e576f83e3c2d1f21e5637c4b5446a828752bb28c
FelipeAlvesGarcia <134567205+FelipeAlvesGarcia@users.noreply.github.com>
2024-07-14 14:13:53 -0300

    Add files via upload

---
+++ Banco(vetor de usuarios)/CC.java
@@ -0,0 +1,27 @@
+package Aula17.Banco2;
+
+public class CC extends Conta2{
+    private float limite;
+    
+    //
+    
+    public boolean sacar(float valor){
+        boolean status = false;
+        if(valor <= saldo){
+            saldo -= valor;
+            status = true;
+        }
+        return status;
+    }
+    
+    //
+    
+    public CC(String nome, String senha, float saldo, float limite){
+        super(nome, senha, saldo);
+        this.limite = limite;
+    }
+    
+    public float getLimite(){
+        return limite;
+    }
+}
